dde1aa61bf53170f5591631a378e6d8ce3a8dcda
LasithEranga <lasitheranga1@gmail.com>
2025-03-24 22:42:05 +0530

    finalize qr scan

--- src/pages/Student/QRScanner.tsx
+++ src/pages/Student/QRScanner.tsx
@@ -1,27 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import QRScannerComponent from "../../components/Student/QR";
 import { request } from "../../lib/apiManager";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
 
 const QRScanner = () => {
   const [scanResult, setScanResult] = useState<string | null>(null);
   const profile = JSON.parse(localStorage.getItem("profile") || "{}");
+  const [attendance, setAttendance] = useState(null);
+  const navigate = useNavigate();
 
-  const markAttendance = async () => {
+  console.log(scanResult);
+
+
+  const markAttendance = async (value: string | null) => {
     try {
-      const response = await request({
-        method: "post",
-        path: "/attendance",
-        requestBody: {
-          studentId: profile.id,
-          qrCode: "M8MXUC3W-81MIJR",
-          remarks: "Attended on time",
-        },
-      });
-      console.log(response);
+      if (typeof value === "string" && value.length > 3) {
+        setScanResult(value);
+        const response = await request({
+          method: "post",
+          path: "/attendance",
+          requestBody: {
+            studentId: profile.id,
+            qrCode: value,
+            remarks: "Attended on time",
+          },
+        });
+        console.log(response);
+        setAttendance(response.data);
+        setScanResult(value);
+      }
     } catch (error) {
-      console.error("Error fetching modules:", error);
+      if (error instanceof Error) {
+        alert(error.message);
+      }
     }
   };
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout | null = null;
+
+    if (attendance) {
+      timeout = setTimeout(() => {
+        navigate('/student/attendence');
+      }, 5000);
+    }
+
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [attendance]);
 
   return (
     <div className="flex flex-col items-center justify-between min-h-screen bg-gray-100 p-4">
@@ -34,19 +64,49 @@ const QRScanner = () => {
         <h1 className="text-2xl font-bold text-gray-800 mb-6">
           Hi Student! 🎓
         </h1>
+        {scanResult === null ? (
+          <div className="w-[320px] h-[340px] bg-white rounded-2xl shadow-lg flex items-center justify-center p-4">
+            <QRScannerComponent onScan={markAttendance} />
+          </div>
+        ) : (
+          <div className="mt-6 p-4 w-[320px] bg-green-100 border border-green-500 text-green-700 rounded-lg shadow-md">
+            <h2 className=" text-center text-xl mb-2">
+              Attendance Marked Successfully
+            </h2>
+            <p className="text-center">
+              Student: {attendance?.student.first_name}{" "}
+              {attendance?.student.last_name}
+              <br />
+              Teacher:{" "}
+              {attendance?.schedule?.subject?.teacher?.first_name +
+                " " +
+                attendance?.schedule?.subject?.teacher?.last_name}
+              <br />
+              Course: {attendance?.schedule?.subject?.subject_name} <br />
+              Date :{" "}
+              {moment(attendance?.schedule.class_date).format("DD-MM-YYYY")}
+              <br />
+              Time:{" "}
+              {moment(attendance?.schedule.start_time, "HH:mm").format(
+                "h:mm A"
+              )}{" "}
+              -{" "}
+              {moment(attendance?.schedule.end_time, "HH:mm").format("h:mm A")}
+            </p>
 
-        <div className="w-[320px] h-[340px] bg-white rounded-2xl shadow-lg flex items-center justify-center p-4">
-          <QRScannerComponent onScan={setScanResult} />
-        </div>
-
-        <button onClick={markAttendance}>markAttendance</button>
-
-        {scanResult && (
-          <div className="mt-6 p-4 w-[320px] bg-green-100 border border-green-500 text-green-700 text-center rounded-lg shadow-md">
-            <p className="font-medium">Scanned Code:</p>
-            <strong className="text-lg">{scanResult}</strong>
+            <p className="text-center text-blue-600">
+              Please wait... You will be redirected to the attendance page.
+            </p>
           </div>
         )}
+
+        {/* <button
+          onClick={() => {
+            markAttendance("M8MXUC3W-81MIJR");
+          }}
+        >
+          markAttendance
+        </button> */}
       </div>
     </div>
   );
